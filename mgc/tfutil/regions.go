04761f7cbe564859167e247ce3c3661543c35fa9
Gefferson Ferraz <me@gefferson.com.br>
2025-03-26 16:35:01 -0300

    feat: flexible url (#133)
    * feat: flexible url

--- mgc/tfutil/regions.go
+++ mgc/tfutil/regions.go
@@ -6,22 +6,28 @@ import (
 	"github.com/MagaluCloud/mgc-sdk-go/client"
 )
 
-const preProdUrl = "https://api.pre-prod.jaxyendy.com"
+const (
+	URL_PRE_PROD = "https://api.pre-prod.jaxyendy.com"
+	URL_DEV_QA   = "https://api.dev-qa.jaxyendy.com"
+	ENV_PROD     = "prod"
+	ENV_PRE_PROD = "pre-prod"
+	ENV_DEV_QA   = "dev-qa"
+)
 
 var regions = map[string]map[string]string{
-	"prod": {
+	ENV_PROD: {
 		"br-ne1":  client.BrNe1.String(),
 		"br-mgl1": client.BrMgl1.String(),
 		"br-se1":  client.BrSe1.String(),
 	},
-	"pre-prod": {
+	ENV_PRE_PROD: {
 		"br-ne1":  buildPreProdUrl("br-ne1"),
 		"br-mgl1": buildPreProdUrl("br-mgl1"),
 		"br-se1":  buildPreProdUrl("br-se1"),
 	},
 }
 var s3Regions = map[string]map[string]string{
-	"prod": {
+	ENV_PROD: {
 		"br-ne1":  "br-ne1.magaluobjects.com",
 		"br-mgl1": "br-se-1.magaluobjects.com",
 		"br-se1":  "br-se1.magaluobjects.com",
@@ -29,17 +35,31 @@ var s3Regions = map[string]map[string]string{
 }
 
 func RegionToUrl(region string, env string) string {
-	if regions[env] == nil {
-		env = "prod"
+	if env == ENV_DEV_QA {
+		return buildQAUrl(region)
 	}
+
+	if regions[env] == nil {
+		env = ENV_PROD
+	}
+
 	if regions[env][region] == "" {
 		region = "br-se1"
 	}
+
 	return regions[env][region]
+
 }
 
-func buildPreProdUrl(env string) string {
-	return preProdUrl + "/" + env
+func buildPreProdUrl(region string) string {
+	return URL_PRE_PROD + "/" + region
+}
+
+func buildQAUrl(region string) string {
+	if region == "" {
+		return URL_DEV_QA
+	}
+	return URL_DEV_QA + "/" + region
 }
 
 func RegionToS3Url(region string, env string) (string, error) {
