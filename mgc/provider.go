b65bf53516edc6203f279afd9d2a76a58e65de79
Luiz Cavalcanti <luiz.cavalcanti@luizalabs.com>
2025-07-03 18:31:46 -0300

    chore: enable dbaas cluster resource and datasource

--- mgc/provider.go
+++ mgc/provider.go
@@ -211,8 +211,8 @@ func (p *mgcProvider) DataSources(ctx context.Context) []func() datasource.DataS
 		datasources.NewDataSourceDdbaasParameterGroup,
 		datasources.NewDataSourceDdbaasParameterGroups,
 		datasources.NewDataSourceDbParametersList,
-		// datasources.NewDBaaSClusterDataSource,
-		// datasources.NewDBaaSClustersDataSource,
+		datasources.NewDBaaSClusterDataSource,
+		datasources.NewDBaaSClustersDataSource,
 	}
 }
 
