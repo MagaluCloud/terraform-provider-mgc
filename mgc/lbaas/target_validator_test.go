6a68b92f79ad0ce140d081f5ead4941ad74065a9
Felipe Publio <felipe@publio.dev>
2025-10-10 10:15:26 -0300

    fix: lbaas target type validation now ignore for unknow values

--- mgc/lbaas/target_validator_test.go
+++ mgc/lbaas/target_validator_test.go
@@ -1,8 +1,14 @@
 package lbaas
 
 import (
+	"context"
 	"testing"
 
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/stretchr/testify/assert"
 )
@@ -40,4 +46,113 @@ func TestValidateTLSCertNameForProtocol_UnknownProtocol(t *testing.T) {
 
 	_, _, has = validateTLSCertNameForProtocol(types.StringValue(""), types.StringValue("web-ssl-cert"))
 	assert.False(t, has)
+}
+
+func makeConfig(t *testing.T, targetsType string) tfsdk.Config {
+	return tfsdk.Config{}
+}
+
+func runTargetValidator(t *testing.T, targetsType string, set types.Set) diag.Diagnostics {
+	cfg := makeConfig(t, targetsType)
+	req := validator.SetRequest{
+		Path:        path.Root("targets"),
+		Config:      cfg,
+		ConfigValue: set,
+	}
+	resp := &validator.SetResponse{
+		Diagnostics: diag.Diagnostics{},
+	}
+	TargetValidator{}.ValidateSet(context.Background(), req, resp)
+	return resp.Diagnostics
+}
+
+func TestTargetValidator_UnknownSet_NoDiagnostics(t *testing.T) {
+	elementType := types.ObjectType{AttrTypes: map[string]attr.Type{
+		"nic_id":     types.StringType,
+		"ip_address": types.StringType,
+		"port":       types.Int64Type,
+	}}
+	unknownSet := types.SetUnknown(elementType)
+	diags := runTargetValidator(t, "raw", unknownSet)
+	assert.False(t, diags.HasError())
+}
+
+func TestTargetValidator_RawType_MissingIPAddress(t *testing.T) {
+	elementType := types.ObjectType{AttrTypes: map[string]attr.Type{
+		"nic_id":     types.StringType,
+		"ip_address": types.StringType,
+		"port":       types.Int64Type,
+	}}
+	elem, _ := types.ObjectValue(map[string]attr.Type{
+		"nic_id":     types.StringType,
+		"ip_address": types.StringType,
+		"port":       types.Int64Type,
+	}, map[string]attr.Value{
+		"nic_id":     types.StringNull(),
+		"ip_address": types.StringNull(),
+		"port":       types.Int64Value(80),
+	})
+	set := types.SetValueMust(elementType, []attr.Value{elem})
+	diags := runTargetValidator(t, "raw", set)
+	assert.False(t, diags.HasError())
+}
+
+func TestTargetValidator_RawType_WithNICIDInvalid(t *testing.T) {
+	elementType := types.ObjectType{AttrTypes: map[string]attr.Type{
+		"nic_id":     types.StringType,
+		"ip_address": types.StringType,
+		"port":       types.Int64Type,
+	}}
+	elem, _ := types.ObjectValue(map[string]attr.Type{
+		"nic_id":     types.StringType,
+		"ip_address": types.StringType,
+		"port":       types.Int64Type,
+	}, map[string]attr.Value{
+		"nic_id":     types.StringValue("nic-1"),
+		"ip_address": types.StringValue("10.0.0.1"),
+		"port":       types.Int64Value(80),
+	})
+	set := types.SetValueMust(elementType, []attr.Value{elem})
+	diags := runTargetValidator(t, "raw", set)
+	assert.False(t, diags.HasError())
+}
+
+func TestTargetValidator_InstanceType_MissingNICID(t *testing.T) {
+	elementType := types.ObjectType{AttrTypes: map[string]attr.Type{
+		"nic_id":     types.StringType,
+		"ip_address": types.StringType,
+		"port":       types.Int64Type,
+	}}
+	elem, _ := types.ObjectValue(map[string]attr.Type{
+		"nic_id":     types.StringType,
+		"ip_address": types.StringType,
+		"port":       types.Int64Type,
+	}, map[string]attr.Value{
+		"nic_id":     types.StringNull(),
+		"ip_address": types.StringNull(),
+		"port":       types.Int64Value(443),
+	})
+	set := types.SetValueMust(elementType, []attr.Value{elem})
+	diags := runTargetValidator(t, "instance", set)
+	assert.False(t, diags.HasError())
+}
+
+func TestTargetValidator_InstanceType_WithIPAddressInvalid(t *testing.T) {
+	elementType := types.ObjectType{AttrTypes: map[string]attr.Type{
+		"nic_id":     types.StringType,
+		"ip_address": types.StringType,
+		"port":       types.Int64Type,
+	}}
+	elem, _ := types.ObjectValue(map[string]attr.Type{
+		"nic_id":     types.StringType,
+		"ip_address": types.StringType,
+		"port":       types.Int64Type,
+	}, map[string]attr.Value{
+		"nic_id":     types.StringValue("nic-1"),
+		"ip_address": types.StringValue("10.0.0.2"),
+		"port":       types.Int64Value(8080),
+	})
+	set := types.SetValueMust(elementType, []attr.Value{elem})
+	diags := runTargetValidator(t, "instance", set)
+	assert.False(t, diags.HasError())
 }
