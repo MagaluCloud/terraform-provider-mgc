a481d157a1e9711a59c81e2820fa57e2004f7676
Felipe Canton Públio <felipe@publio.dev>
2025-10-21 18:36:03 -0300

    feat: improve virtual machine instances creation with more network fields (#229)
    * feat: improve virtual machine instances creation with more network fields
    
    * Remove support for custom network_interface_id on VM creation
    
    This deprecates the ability to specify network_interface_id when
    creating a virtual machine instance. All VMs now use automatically
    managed primary interfaces. Documentation and tests updated to reflect
    the change.

--- examples/resources/mgc_virtual_machine_instances/resource.tf
+++ examples/resources/mgc_virtual_machine_instances/resource.tf
@@ -1,28 +1,61 @@
-resource "mgc_virtual_machine_instances" "tc1_basic_instance" {
+resource "mgc_virtual_machine_instances" "basic_instance" {
   name         = "basic-instance-name"
   machine_type = "BV1-1-40"
   image        = "cloud-ubuntu-24.04 LTS"
   ssh_key_name = "your-ssh-key-name"
 }
 
-resource "mgc_virtual_machine_instances" "tc2_instance_with_az" {
-  name              = "tc2-instance-with-az"
+resource "mgc_virtual_machine_instances" "instance_with_az" {
+  name              = "instance-with-az"
   availability_zone = "br-ne1-a"
   machine_type      = "BV4-8-100"
   image             = "cloud-ubuntu-24.04 LTS"
   ssh_key_name      = "your-ssh-key-name"
 }
 
-resource "mgc_virtual_machine_instances" "tc3_instance_with_usardata" {
-  name         = "tc3-instance-with-userdata"
+resource "mgc_virtual_machine_instances" "instance_with_usardata" {
+  name         = "instance-with-userdata"
   machine_type = "BV4-8-100"
   image        = "cloud-ubuntu-24.04 LTS"
   ssh_key_name = "your-ssh-key-name"
   user_data    = base64encode("#!/bin/bash\necho 'Hello, World!'")
 }
 
-resource "mgc_virtual_machine_instances" "tc4_instance_with_windows" {
-  name         = "tc4-instance-with-windows"
+resource "mgc_virtual_machine_instances" "instance_with_windows" {
+  name         = "instance-with-windows"
   machine_type = "BV4-8-100"
   image        = "windows-server-2022"
+}
+
+resource "mgc_virtual_machine_instances" "instance_with_custom_interface" {
+  name                 = "instance-with-custom-interface"
+  machine_type         = "BV2-4-10"
+  image                = "cloud-ubuntu-24.04 LTS"
+  ssh_key_name         = "your-ssh-key-name"
+  network_interface_id = mgc_network_vpcs_interfaces.custom_interface.id
+}
+
+resource "mgc_virtual_machine_instances" "instance_with_public_ipv4" {
+  name                 = "instance-with-public-ipv4"
+  machine_type         = "BV2-4-10"
+  image                = "cloud-ubuntu-24.04 LTS"
+  ssh_key_name         = "your-ssh-key-name"
+  allocate_public_ipv4 = true
+}
+
+resource "mgc_virtual_machine_instances" "instance_with_security_groups" {
+  name                     = "instance-with-security-groups"
+  machine_type             = "BV2-4-10"
+  image                    = "cloud-ubuntu-24.04 LTS"
+  ssh_key_name             = "your-ssh-key-name"
+  creation_security_groups = [mgc_network_security_groups.security_group.id]
+}
+
+resource "mgc_virtual_machine_instances" "instance_with_security_groups_and_public_ipv4" {
+  name                     = "instance_with_security_groups_and_public_ipv4"
+  machine_type             = "BV2-4-10"
+  image                    = "cloud-ubuntu-24.04 LTS"
+  ssh_key_name             = "your-ssh-key-name"
+  allocate_public_ipv4     = true
+  creation_security_groups = [mgc_network_security_groups.security_group.id]
 }
--- tests/virtual-machine/main.tf
+++ tests/virtual-machine/main.tf
@@ -8,7 +8,7 @@ variable "instance_name" {
 variable "machine_type" {
   description = "Machine type for the test instance"
   type        = string
-  default     = "BV1-1-40"
+  default     = "BV1-2-40"
 }
 
 resource "mgc_ssh_keys" "ssh_key" {
@@ -16,7 +16,7 @@ resource "mgc_ssh_keys" "ssh_key" {
   key  = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAKWxY1opxbh0LNTNeAHORTZEHdPRFvrfcXyhZQlsR7t"
 }
 
-# Test Case 1: Basic VM Instance
+# # Test Case 1: Basic VM Instance
 resource "mgc_virtual_machine_instances" "tc1_basic_instance" {
   name              = var.instance_name
   availability_zone = "br-ne1-a"
@@ -30,7 +30,7 @@ resource "mgc_virtual_machine_instances" "tc1_basic_instance" {
   }
 }
 
-# Test Case 2: VM Instance with Availability Zone
+# # Test Case 2: VM Instance with Availability Zone
 resource "mgc_virtual_machine_instances" "tc2_instance_with_az" {
   name              = "tc2-instance-with-az"
   availability_zone = "br-ne1-a"
@@ -42,7 +42,40 @@ resource "mgc_virtual_machine_instances" "tc2_instance_with_az" {
   depends_on = [mgc_virtual_machine_instances.tc1_basic_instance]
 }
 
-# Data Sources for Validation
+# Test Case 3: VM Instance with custom interface
+resource "mgc_network_vpcs_interfaces" "custom_interface" {
+  name   = "custom-pip-interface"
+  vpc_id = "144e5176-5a75-4afc-ae75-38160f9fd21d"
+}
+
+resource "mgc_virtual_machine_instances" "instance_with_custom_interface" {
+  name         = "tc4-instance-with-custom-interface-hc"
+  machine_type = var.machine_type
+  image        = "cloud-ubuntu-24.04 LTS"
+  ssh_key_name = mgc_ssh_keys.ssh_key.name
+  # network_interface_id = mgc_network_vpcs_interfaces.custom_interface.id
+}
+
+resource "mgc_virtual_machine_instances" "instance_with_pip" {
+  name                 = "tc4-instance-with-public-ip"
+  machine_type         = var.machine_type
+  image                = "cloud-ubuntu-24.04 LTS"
+  ssh_key_name         = mgc_ssh_keys.ssh_key.name
+  allocate_public_ipv4 = true
+}
+resource "mgc_network_security_groups" "security_group" {
+  name = "auxiliary-security-group-vm-a"
+}
+
+resource "mgc_virtual_machine_instances" "instance_with_sg" {
+  name                     = "tc4-instance-with-sg"
+  machine_type             = var.machine_type
+  image                    = "cloud-ubuntu-24.04 LTS"
+  ssh_key_name             = mgc_ssh_keys.ssh_key.name
+  creation_security_groups = [mgc_network_security_groups.security_group.id]
+}
+
+# # Data Sources for Validation
 data "mgc_virtual_machine_instance" "tc1_validation" {
   id = mgc_virtual_machine_instances.tc1_basic_instance.id
 }
@@ -51,127 +84,11 @@ data "mgc_virtual_machine_instance" "tc2_validation" {
   id = mgc_virtual_machine_instances.tc2_instance_with_az.id
 }
 
-# List Resources for Testing
+data "mgc_virtual_machine_instance" "tc4_validation" {
+  id = mgc_virtual_machine_instances.instance_with_custom_interface.id
+}
+
+# # List Resources for Testing
 data "mgc_virtual_machine_instances" "all_instances" {}
 data "mgc_virtual_machine_images" "available_images" {}
 data "mgc_virtual_machine_types" "available_types" {}
-
-# Test Outputs
-output "test_case_1_validation" {
-  description = "Validation output for basic instance test case"
-  value = {
-    instance_id   = data.mgc_virtual_machine_instance.tc1_validation.id
-    instance_name = data.mgc_virtual_machine_instance.tc1_validation.name
-    status        = data.mgc_virtual_machine_instance.tc1_validation.status
-    az            = data.mgc_virtual_machine_instance.tc1_validation.availability_zone
-    instace_type  = data.mgc_virtual_machine_instance.tc1_validation.machine_type_id
-  }
-}
-
-output "test_case_2_validation" {
-  description = "Validation output for AZ instance test case"
-  value = {
-    instance_id   = data.mgc_virtual_machine_instance.tc2_validation.id
-    instance_name = data.mgc_virtual_machine_instance.tc2_validation.name
-    status        = data.mgc_virtual_machine_instance.tc2_validation.status
-    az            = data.mgc_virtual_machine_instance.tc2_validation.availability_zone
-    instance_type = data.mgc_virtual_machine_instance.tc2_validation.machine_type_id
-  }
-}
-
-output "all_vm_instances" {
-  description = "All VM Instances for testing"
-  value       = data.mgc_virtual_machine_instances.all_instances
-}
-
-output "tc1_basic_instance_details" {
-  value = {
-    created_at         = mgc_virtual_machine_instances.tc1_basic_instance.created_at
-    vpc_id             = mgc_virtual_machine_instances.tc1_basic_instance.vpc_id
-    network_interfaces = mgc_virtual_machine_instances.tc1_basic_instance.network_interfaces
-  }
-}
-
-output "tc2_instance_with_az_details" {
-  value = {
-    created_at         = mgc_virtual_machine_instances.tc2_instance_with_az.created_at
-    vpc_id             = mgc_virtual_machine_instances.tc2_instance_with_az.vpc_id
-    network_interfaces = mgc_virtual_machine_instances.tc2_instance_with_az.network_interfaces
-  }
-}
-
-# Additional outputs for test validation
-output "vm_instance_details" {
-  description = "Virtual Machine Instance Details"
-  value = {
-    machine_type = {
-      id   = data.mgc_virtual_machine_instance.tc1_validation.machine_type_id
-      name = data.mgc_virtual_machine_instance.tc1_validation.machine_type_name
-      ram  = data.mgc_virtual_machine_instance.tc1_validation.machine_type_ram
-      vcpu = data.mgc_virtual_machine_instance.tc1_validation.machine_type_vcpus
-      disk = data.mgc_virtual_machine_instance.tc1_validation.machine_type_disk
-    }
-    image = {
-      id       = data.mgc_virtual_machine_instance.tc1_validation.image_id
-      name     = data.mgc_virtual_machine_instance.tc1_validation.image_name
-      platform = data.mgc_virtual_machine_instance.tc1_validation.image_platform
-    }
-    network = {
-      vpc_id   = data.mgc_virtual_machine_instance.tc1_validation.vpc_id
-      vpc_name = data.mgc_virtual_machine_instance.tc1_validation.vpc_name
-    }
-  }
-}
-
-output "tc1_details" {
-  value = {
-    id                 = mgc_virtual_machine_instances.tc1_basic_instance.id
-    name               = mgc_virtual_machine_instances.tc1_basic_instance.name
-    created_at         = mgc_virtual_machine_instances.tc1_basic_instance.created_at
-    vpc_id             = mgc_virtual_machine_instances.tc1_basic_instance.vpc_id
-    machine_type       = mgc_virtual_machine_instances.tc1_basic_instance.machine_type
-    network_interfaces = mgc_virtual_machine_instances.tc1_basic_instance.network_interfaces
-  }
-}
-
-output "tc2_details" {
-  value = {
-    id                 = mgc_virtual_machine_instances.tc2_instance_with_az.id
-    name               = mgc_virtual_machine_instances.tc2_instance_with_az.name
-    created_at         = mgc_virtual_machine_instances.tc2_instance_with_az.created_at
-    vpc_id             = mgc_virtual_machine_instances.tc2_instance_with_az.vpc_id
-    machine_type       = mgc_virtual_machine_instances.tc2_instance_with_az.machine_type
-    network_interfaces = mgc_virtual_machine_instances.tc2_instance_with_az.network_interfaces
-  }
-}
-
-output "vm_instance_validation" {
-  description = "Complete validation of VM instance fields"
-  value = {
-    basic_instance = {
-      id                 = data.mgc_virtual_machine_instance.tc1_validation.id
-      name               = data.mgc_virtual_machine_instance.tc1_validation.name
-      created_at         = data.mgc_virtual_machine_instance.tc1_validation.created_at
-      updated_at         = data.mgc_virtual_machine_instance.tc1_validation.updated_at
-      image_id           = data.mgc_virtual_machine_instance.tc1_validation.image_id
-      image_name         = data.mgc_virtual_machine_instance.tc1_validation.image_name
-      image_platform     = data.mgc_virtual_machine_instance.tc1_validation.image_platform
-      machine_type_id    = data.mgc_virtual_machine_instance.tc1_validation.machine_type_id
-      machine_type_name  = data.mgc_virtual_machine_instance.tc1_validation.machine_type_name
-      machine_type_disk  = data.mgc_virtual_machine_instance.tc1_validation.machine_type_disk
-      machine_type_ram   = data.mgc_virtual_machine_instance.tc1_validation.machine_type_ram
-      machine_type_vcpus = data.mgc_virtual_machine_instance.tc1_validation.machine_type_vcpus
-      vpc_id             = data.mgc_virtual_machine_instance.tc1_validation.vpc_id
-      vpc_name           = data.mgc_virtual_machine_instance.tc1_validation.vpc_name
-      ssh_key_name       = data.mgc_virtual_machine_instance.tc1_validation.ssh_key_name
-      status             = data.mgc_virtual_machine_instance.tc1_validation.status
-      state              = data.mgc_virtual_machine_instance.tc1_validation.state
-      user_data          = data.mgc_virtual_machine_instance.tc1_validation.user_data
-      availability_zone  = data.mgc_virtual_machine_instance.tc1_validation.availability_zone
-      labels             = data.mgc_virtual_machine_instance.tc1_validation.labels
-      error_message      = data.mgc_virtual_machine_instance.tc1_validation.error_message
-      error_slug         = data.mgc_virtual_machine_instance.tc1_validation.error_slug
-      interfaces         = data.mgc_virtual_machine_instance.tc1_validation.interfaces
-    }
-  }
-}
