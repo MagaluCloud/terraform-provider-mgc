77e9dafa2f4b5041c762ce34a3c89dab709394f2
onofre.matheus <matheusonofre@ufu.br>
2026-05-27 12:16:34 -0300

    fix: k8s cluster example with version

--- examples/resources/mgc_kubernetes_cluster/resource.tf
+++ examples/resources/mgc_kubernetes_cluster/resource.tf
@@ -3,5 +3,4 @@ resource "mgc_kubernetes_cluster" "cluster" {
   version              = mgc_kubernetes_version.versions[0].version
   enabled_server_group = false
   description          = "Cluster Example"
-  version              = "v1.35.2"
 }
