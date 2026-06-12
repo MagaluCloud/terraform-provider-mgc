1d535dca1a7405acbaea41561efa35bf67701d07
Einier Freyre Corona <einierfreyre60@gmail.com>
2026-06-12 12:43:58 -0300

    fix: use req.Plan instead of req.Config in DBaaS instance Create method

--- mgc/database/resource_instances.go
+++ mgc/database/resource_instances.go
@@ -303,7 +303,7 @@ func (r *DBaaSInstanceResource) Schema(_ context.Context, _ resource.SchemaReque
 
 func (r *DBaaSInstanceResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
 	var data DBaaSInstanceModel
-	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
 	if resp.Diagnostics.HasError() {
 		return
 	}
